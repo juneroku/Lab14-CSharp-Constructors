6573fd4bfd486d8c3fd90e287212665dba59653c
juneroku <horizondawn2547@gmail.com>
2025-03-29 18:56:10 +0700

    Finish homework

---
+++ Lab13_Ex04/Program.cs
@@ -0,0 +1,42 @@
+﻿var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+    // เปลี่ยน constructor นี้เป็น protected หรือ public
+    protected Shape()  // หรือ public
+    {
+        System.Console.WriteLine("This is some shape with unknown side");
+    }
+    
+    public Shape(int NumOfSide)
+    {
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base() // เรียก constructor ของ Shape ที่ไม่มีพารามิเตอร์
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4) // เรียก constructor ของ Shape ที่มีพารามิเตอร์
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3) // เรียก constructor ของ Shape ที่มีพารามิเตอร์
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
---
+++ Lab13_Ex05/Program.cs
@@ -0,0 +1,44 @@
+﻿var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+
+    // Constructor ที่ไม่มีพารามิเตอร์
+    protected Shape() 
+    {
+    }
+
+    // Constructor ที่รับพารามิเตอร์ NumOfSide
+    public Shape(int NumOfSide) : this() 
+    {
+        this.NumOfSide = NumOfSide;
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base(0) // ระบุจำนวนขอบเป็น 0 สำหรับวงกลม
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4) // ระบุจำนวนขอบเป็น 4 สำหรับสี่เหลี่ยม
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3) // ระบุจำนวนขอบเป็น 3 สำหรับสามเหลี่ยม
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
